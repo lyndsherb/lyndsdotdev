e93f02f34b742c79f812e706b4735e6f07b5c694
Lyndsey Herbert <lyndsherb@gmail.com>
2023-09-13 00:45:14 +0100

    Add current year filter, set Posts instead of About in nav

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,12 @@ module.exports = function(config) {
     config.addPassthroughCopy('css');
     config.addPassthroughCopy({ 'images/favicons': 'images/favicons' });
 
+    config.addFilter('currentYear', function() {
+        const date = new Date();
+
+        return date.getFullYear();
+    });
+
     return {
         dir: {
             layouts: '_layouts',
