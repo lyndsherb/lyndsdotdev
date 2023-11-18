3c994c32aea55e6fae2d17ed3c34b9037948d9f5
Lyndsey Herbert <lyndsherb@gmail.com>
2023-11-18 02:03:25 +0000

    Set template formats

--- .eleventy.js
+++ .eleventy.js
@@ -3,6 +3,7 @@ module.exports = function(config) {
     config.addPassthroughCopy({ 'src/css': 'css' });
 
     return {
+        templateFormats: ['md', '11ty.js'],
         dir: {
             includes: 'src/includes',
             layouts: 'src/layouts',
