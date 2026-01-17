888d0a82a0227954a29f2a8da5cefb99853e61b7
Lyndsey Herbert <lyndsherb@gmail.com>
2026-01-17 01:07:44 +0000

    Add old posts, add initial About page, update config to set build format to directory

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,9 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+  build: {
+    format: "directory",
+  },
   server: {
     allowedHosts: ["stinky-orca-app-pnshi.ondigitalocean.app"]
   }
